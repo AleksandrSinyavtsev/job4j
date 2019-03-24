2c46935d9ed5c72e0f485af5c514fa5295ea01a9
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-03-24 13:59:57 +0300

    1.3.JavaDoc. Code convention.[#121616]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,22 @@
+/**
+* Package for calculate task.
+*
+* @author Aleksandr Sinyavtsev (parsentev@yandex.ru)
+* @version $Id$
+* @since 23.03.2019
+*/
 package ru.job4j.calculate;
-
+/**
+*Class класс для вывода строки aah aah aah
+*@author Aleksandr Sinyavtsev
+*@since 23.03.2019
+*/
 public class Calculate {
-	
+/**
+*Метод для тестирования.
+*@param value строка для вывода в консоль
+*@return String value.
+*/
 	public String echo(String value) {
 		return String.format("%s %s %s", value, value, value);
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Aleksandr Sinyavtsev (parsentev@yandex.ru)
+* @version $Id$
+* @since 23.03.2019
+*/
+package ru.job4j.calculate;
