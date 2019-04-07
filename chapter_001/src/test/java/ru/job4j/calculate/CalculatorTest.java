740d4135bb3156e0d9c96566f9b93e894272dc17
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-04-07 14:33:52 +0300

    3.1. Элементарный калькулятор.[#121606]

--- chapter_001/src/test/java/ru/job4j/calculate/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculatorTest.java
@@ -4,7 +4,16 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+ *Test
+ *@author Aleksandr Sinyavtsev (sinyavtsev.alek@yandex.ru)
+ *@version $Id$
+ *@since 05.04.2019
+ */
 public class CalculatorTest  {
+    /**
+     * Тест сложения
+     */
     @Test
     public void whenAddOnePlusOneThenTwo()  {
         Calculator calc = new Calculator();
@@ -12,6 +21,10 @@ public class CalculatorTest  {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Тест вычитания
+     */
     @Test
     public void whenSubtrackThreeMinusOneThenTwo() {
         Calculator calc = new Calculator();
@@ -19,6 +32,10 @@ public class CalculatorTest  {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Тест деления
+     */
     @Test
     public void whenDiv2On2Then1() {
         Calculator calc = new Calculator();
@@ -26,6 +43,10 @@ public class CalculatorTest  {
         double expected = 1D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * Тест умножения
+     */
     @Test
     public void whenMultiple2On2Then4() {
         Calculator calc = new Calculator();
