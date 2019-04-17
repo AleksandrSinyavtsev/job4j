a633e0a4a58608dad4aeb0c8dac30d11c1593ae9
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-04-17 15:28:17 +0300

    4.0. Операторы ветвлений[#121578]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Driver.java
@@ -0,0 +1,35 @@
+package ru.job4j.condition;
+/**
+ *Driver.
+ *@author Aleksandr Sinyavtsev (sinyavtsev.alek@yandex.ru)
+ *@version $0.1$
+ *@since 17.04.2019
+ */
+public class Driver {
+    private char license = 'N';
+
+    /**
+     * Метод отправляющий водителя на экзамен если лиензии нет.
+     * @param category
+     */
+    public void passExamOn(char category) {
+        this.license = category;
+    }
+
+    /**
+     * Метод оценивающий есть лицензия или нет.
+     * @return
+     */
+    public boolean hasLicense() {
+        return this.license == 'A'|| this.license == 'B'|| this.license == 'C';
+    }
+
+    /**
+     * Метод определяющий может ли водитель сесть за руль.
+     * @param category
+     * @return
+     */
+    public boolean canDrive(char category) {
+        return this.license == category;
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/condition/TruckerJob.java
@@ -0,0 +1,30 @@
+package ru.job4j.condition;
+/**
+ *TruckerJob.
+ *@author Aleksandr Sinyavtsev (sinyavtsev.alek@yandex.ru)
+ *@version $0.1$
+ *@since 17.04.2019
+ */
+public class TruckerJob {
+    /**
+     * Метод определяющий может ли Стив получить работу водителем.
+     * @param args
+     */
+    public static void main(String[] args) {
+        Driver steve = new Driver();
+        System.out.println("Steve wants to trucker job.");
+        if (!steve.hasLicense()) {
+            System.out.println("Steve need to get a license!");
+            steve.passExamOn('A');
+        }
+        if (steve.canDrive('C')) {
+            System.out.println("Steve can works on truckers!");
+        }
+        else if (steve.canDrive('B')) {
+            System.out.println("Steve can works on taxi.");
+        }
+        else {
+           System.out.println("Steve does not have a job, but ride on motorbike!");
+        }
+    }
+}
