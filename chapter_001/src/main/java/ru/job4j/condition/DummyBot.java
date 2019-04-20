6ba5337bd2c8ef014f26fa0609566aac54e86e25
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-04-20 04:53:34 +0300

    4.0. Операторы ветвлений[#121578]

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+/**
+ *DummyBot.
+ *@author Aleksandr Sinyavtsev (sinyavtsev.alek@yandex.ru)
+ *@version $0.1$
+ *@since 20.04.2019
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
+
