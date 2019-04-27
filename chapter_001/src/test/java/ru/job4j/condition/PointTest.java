f7d61452df6d2b7c011a86ab7d55e322c0e7699d
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-04-27 13:35:13 +0300

    4.2. Максимум из двух чисел[#121576]
    4.3. Вычисление площади треугольника[#121579]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+import static org.hamcrest.number.IsCloseTo.closeTo;
+
 public class PointTest {
     /**
      * Тест определения расстояния.
