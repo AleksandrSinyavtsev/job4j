ad49dad784f6c4b3a9049f5a9a1bd8c792096393
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-04-28 14:05:23 +0300

    4.2. Максимум из двух чисел  с дополненым тестом.[#121576]

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -11,4 +11,10 @@ public class MaxTest {
         int result = max.max(1, 2);
         assertThat(result, is(2));
     }
+    @Test
+    public void whenMax2To1Then2() {
+        Max max = new Max();
+        int resalt = max.max(2, 1);
+        assertThat(resalt, is(2));
+    }
 }
