c3104eccb490583926e249c9131e455da516a653
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-07-07 15:57:00 +0300

    Экзамен. Базовый синтаксис.[#121624]

--- chapter_001/src/main/java/ru/job4j/array/ArrayMerge.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayMerge.java
@@ -3,24 +3,25 @@ package ru.job4j.array;
 public class ArrayMerge {
     public int[] merge(int[] left, int[] rigth) {
         int [] merged = new int [left.length + rigth.length];
-        int i = 0;
-        int j = 0;
-            while(i < left.length && j < rigth.length) {
-                if (left[i] < rigth[j]) {
-                    merged[i + j] = left[i];
-                    i++;
-                }
-                 if  (left[i] > rigth[j]){
-                    merged[i + j] = rigth[j];
-                    j++;
-                }
+        for (int i = 0; i <merged.length/2; i++) {
+            if (left[i]>rigth[i]) {
+                merged[i+i] = rigth[i];
+                merged [i+i+1] = left [i];
+            } else {
+                merged [i+i] = left [i];
+                merged [i+i+1] = rigth [i];
+
             }
-             if (i < left.length) {
-            System.arraycopy(left, i, merged, i+j, merged.length - 1 - i - j);
-             }
-            else if (j < rigth.length) {
-            System.arraycopy(rigth, j, merged, i+j, merged.length - 1 - i - j);
+
+        }
+        for (int i=0; i<merged.length-1; i++) {
+            int t = merged[i];
+            if (merged[i]>merged[i+1]) {
+                merged[i] = merged[i+1];
+                merged [i+1] = t;
             }
+
+        }
         return merged;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/ArrayMergeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayMergeTest.java
@@ -9,10 +9,10 @@ public class ArrayMergeTest {
     @Test
     public void whenArrayMerged() {
         ArrayMerge array = new ArrayMerge();
-        int[] left = new int[]{1, 2, 4, 6, 11};
+        int[] left = new int[]{1, 2, 4, 6};
         int[] rigth = new int[] {3, 5, 8, 10};
         int[] result = array.merge(left, rigth);
-        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 8, 10, 11};
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 8, 10};
         assertThat(result, is(expect));
     }
 }
