5de05addd2afa74d307af3609c2388d8ad36b294
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-07-12 13:25:49 +0300

    Экзамен. Базовый синтаксис.[#121624]

--- chapter_001/src/test/java/ru/job4j/array/ArrayMergeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayMergeTest.java
@@ -15,4 +15,31 @@ public class ArrayMergeTest {
         int[] expect = new int[]{1, 2, 3, 4, 5, 6, 8, 10, 11};
         assertThat(result, is(expect));
     }
+    @Test
+    public void whenArrayMerged1to10() {
+        ArrayMerge array = new ArrayMerge();
+        int[] left = new int[]{1, 2, 3, 4, 5};
+        int[] rigth = new int[] {6, 7, 8, 9, 10};
+        int[] result = array.merge(left, rigth);
+        int[] expect = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayMerged1to2() {
+        ArrayMerge array = new ArrayMerge();
+        int[] left = new int[]{1, 1, 1, 1, 1};
+        int[] rigth = new int[] {1, 1, 1, 1, 2};
+        int[] result = array.merge(left, rigth);
+        int[] expect = new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 2};
+        assertThat(result, is(expect));
+    }
+    @Test
+    public void whenArrayNegativeAndPozitiveMerged() {
+        ArrayMerge array = new ArrayMerge();
+        int[] left = new int[]{-5, -4, -3, -2, -1};
+        int[] rigth = new int[] {0, 1, 2, 3, 4, 5};
+        int[] result = array.merge(left, rigth);
+        int[] expect = new int[]{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
 }
