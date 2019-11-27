a141c922b53a3298ac26b868f7836685601a64cd
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-11-27 16:12:40 +0300

    1.1. Создание объекта [#165452]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,16 @@
+package ru.job4j.oop;
+
+/**
+ * Kласс описывает Dog.
+ */
+public class Dog {
+    /**
+     * метод создает 3 объекта Dog.
+     * @param args дефолтный.
+     */
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}
