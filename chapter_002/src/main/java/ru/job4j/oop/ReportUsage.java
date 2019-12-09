e1e55b5dda75f267266af547e72e30b7fc0334b6
AleksandrSinyavtsev <sinyavtsev.alek@yandex.ru>
2019-12-09 15:27:44 +0300

    5. Аннотация @Override [#174891]

--- chapter_002/src/main/java/ru/job4j/oop/ReportUsage.java
+++ chapter_002/src/main/java/ru/job4j/oop/ReportUsage.java
@@ -3,7 +3,11 @@ package ru.job4j.oop;
 public class ReportUsage {
     public static void main(String[] args) {
        JSONReport report = new JSONReport();
+       HtmlReport htmlReport = new HtmlReport();
+       String text1 = htmlReport.generate("Report's name", "Report's body");
         String text = report.generate("Report's name", "Report's body");
         System.out.println(text);
+        System.out.println(text1);
+
     }
 }
