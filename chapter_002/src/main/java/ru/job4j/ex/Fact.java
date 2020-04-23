89fef8ebaf65c395252f0cf08c6c86a37892dc42
Aleksandr Sinyavtsev <sinyavtsev.alek@yandex.ru>
2020-04-23 22:55:49 +0300

    0.2. Кидаем исключение - throw new RuntimeException [#219365]

---
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact f = new Fact();
+        System.out.println(f.calc(0));
+    }
+
+    public int calc(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("n can't have a negative value.");
+        }
+        int rsl = 1;
+        for (int index = 1; index <= n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
